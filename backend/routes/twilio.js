7c2f374a2715122762b6625a01a076d3373d1d3f
SHREYAS <shreyask72201@gmail.com>
2025-11-17 12:40:54 +0530

    t

--- backend/routes/twilio.js
+++ backend/routes/twilio.js
@@ -6,15 +6,16 @@ const { AccessToken } = jwt;
 const { VideoGrant } = AccessToken;
 
 const router = express.Router();
-console.log("TOKEN CHECK");
-console.log("ACCOUNT:", ACCOUNT_SID);
-console.log("KEY SID:", API_KEY_SID);
-console.log("KEY SECRET LENGTH:", API_KEY_SECRET?.length);
+
 
 
 const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const API_KEY_SID = process.env.TWILIO_API_KEY_SID;
 const API_KEY_SECRET = process.env.TWILIO_API_KEY_SECRET;
+console.log("TOKEN CHECK");
+console.log("ACCOUNT:", ACCOUNT_SID);
+console.log("KEY SID:", API_KEY_SID);
+console.log("KEY SECRET LENGTH:", API_KEY_SECRET?.length);
 
 router.get("/token", (req, res) => {
   const identity = req.query.identity;
